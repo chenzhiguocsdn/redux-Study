61dc9f5663758f8888085d1ba511b9979de76034
chenzhiguo <1090236106@qq.com>
2022-10-17 23:15:25 +0800

    提升渲染性能 作者：陈志国

--- src/features/users/UserPage.js
+++ src/features/users/UserPage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { selectAllPosts } from '../posts/postSlice'
+import { selectPostsByUser } from '../posts/postSlice'
 import { selectUserById } from './usersSlice'
  
 export const UserPage = ({ match }) => {
@@ -9,10 +9,7 @@ export const UserPage = ({ match }) => {
 
   const user = useSelector((state) => selectUserById(state, userId))
 
-  const postsForUser = useSelector((state) => {
-    const allPosts = selectAllPosts(state)
-    return allPosts.filter((post) => post.user === userId)
-  })
+  const postsForUser = useSelector(state => selectPostsByUser(state, userId))
 
   const postTitles = postsForUser.map((post) => (
     <li key={post.id}>
